15edae903b7f281d48e1eea0465e5af44d419565
awesome-edition-6075 <zachpardee@gmail.com>
2019-03-10 20:44:06 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,10 @@
 class Hash
   def keys_of(arguments)
-    # code goes here
+    new_array = []
+    collect {|key, value|
+      if arguments.include?(value)
+        new_array << key
+      end}
+      return new_array.flatten
   end
 end
